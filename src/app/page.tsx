58a91a1f7bb62ac80187446ec53ed69a50c46c7a
Christian Malimit <christianmalimit07@gmail.com>
2025-06-24 18:58:54 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 /* File: /app/page.tsx */
 'use client';
-import Image from 'next/image';
 import Link from 'next/link';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
